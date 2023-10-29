3be4f3712a6040b884dc7c1b33f7d5220cde7309
Vinita Asari <vinita.asari@jump360.me>
2023-10-29 23:00:37 +0530

    changes

--- src/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Button({ label, onClick }) {
-  return (
-    <span>hey</span>
-  );
-}
-
-export default Button;
---
+++ src/Greet.jsx
@@ -0,0 +1,8 @@
+// src/Greet.js
+import React from 'react';
+
+const Greet = ({ name }) => {
+  return <h1>Hello, {name}!</h1>;
+};
+
+export default Greet;
